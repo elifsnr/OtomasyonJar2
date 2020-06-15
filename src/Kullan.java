adced764333b8d20fcc815e32de3dfd2196eda4a
Gürcan ATAMAN <gurcanataman@gmail.com>
2020-06-15 23:39:59 +0300

    Initial commit

---
+++ src/Kullan.java
@@ -0,0 +1,10 @@
+  import com.akahraman.cikarma.Islem;
+public class Kullan {
+    public static void main(String [] args) throws InterruptedException{ 
+ Islem i1=new Islem();
+ 
+        
+        
+    }
+  
+}
